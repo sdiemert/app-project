c2720090988eeb22f78c69ba6105fca799f2d1f4
Simon Diemert <simon.diemert@gmail.com>
2016-08-15 17:02:21 -0700

    minor change to namespace, still works

--- app-project/src/app_project/db.clj
+++ app-project/src/app_project/db.clj
@@ -1,13 +1,10 @@
  (ns app-project.db
    (:require [grape.core :refer :all]))
 
-(use 'grape.core)
-
 (def GTSystem (gts 'app {}))
 
 (def GTSystem (rule 'doAction! []
-      {:create (pattern (node 'n1 {:label "AppNode"}))} GTSystem))
-
+{:create (pattern (node 'n1 {:label "AppNode"}))} GTSystem))
 
 (defn doAction []
   (attempt GTSystem (apl 'doAction!))
