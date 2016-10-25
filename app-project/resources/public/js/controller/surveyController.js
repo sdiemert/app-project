7fa4624d4ab3a4a93481eda10fc12701dc05ca1d
Simon Diemert <simon.diemert@gmail.com>
2016-10-25 10:43:51 -0700

    fading of questions complete.

--- app-project/resources/public/js/controller/surveyController.js
+++ app-project/resources/public/js/controller/surveyController.js
@@ -4,25 +4,38 @@ function surveyController ($scope, $rootScope, $state, apiService){
 
     var BEFORE_SURVEY_START = 0, BEFORE_SURVEY_END = 12, AFTER_SURVEY_START = 13, AFTER_SURVEY_END = 14;
 
+    var FADE_TIME = 300;
+
     $scope.showQuestion = function(x){
-        $("#q"+x).show();
+        $("#q"+x).add($("#next-button")).add($("#back-button")).fadeIn(FADE_TIME);
     };
 
-    $scope.hideAllQuestions = function(){
-        for(var i = 0; i < $scope.qNums.length; i++){
+    $scope.hideAllQuestions = function(f){
+
+        for(var i = 1; i < $scope.qNums.length; i++){
             $("#q"+$scope.qNums[i]).hide();
             $("#q"+$scope.qNums[i]+"-other").hide();
         }
+
+        $("#next-button").hide();
+        $("#back-button").hide();
+
     };
 
     $scope.showOther = function(x){
 
         var v = $("#q"+x+"-input").val();
         if(v === "other" || v === "yes"){
-            $("#q"+x+"-other").show();
+            $("#q"+x+"-other").fadeIn(FADE_TIME);
         }else{
-            $("#q"+x+"-other").hide();
+            $("#q"+x+"-other").fadeOut(FADE_TIME);
         }
+    };
+
+    $scope.fadeOutQuestion = function(x, cb){
+        $("#q"+x).fadeOut(FADE_TIME);
+        $("#next-button").fadeOut(FADE_TIME);
+        $("#back-button").fadeOut(FADE_TIME, cb);
     };
 
     $scope.init = function(){
@@ -37,29 +50,35 @@ function surveyController ($scope, $rootScope, $state, apiService){
         $scope.hideAllQuestions();
         $scope.showQuestion($scope.qNums[$scope.questionNum]);
 
+
     };
 
     $scope.goBack = function(){
 
-        if($scope.questionNum === BEFORE_SURVEY_START){
-            return $state.go("survey", {questionNum : BEFORE_SURVEY_START});
-        }else if($scope.questionNum === AFTER_SURVEY_START){
-            return $state.go("survey", {questionNum : AFTER_SURVEY_END});
-        }else{
-            return $state.go("survey", {questionNum: $scope.questionNum - 1});
-        }
+        $scope.fadeOutQuestion($scope.qNums[$scope.questionNum], function() {
+            if ($scope.questionNum === BEFORE_SURVEY_START) {
+                return $state.go("survey", {questionNum: BEFORE_SURVEY_START});
+            } else if ($scope.questionNum === AFTER_SURVEY_START) {
+                return $state.go("survey", {questionNum: AFTER_SURVEY_END});
+            } else {
+                return $state.go("survey", {questionNum: $scope.questionNum - 1});
+            }
+        });
 
     };
 
     $scope.goNext = function(){
 
-        if($scope.questionNum === BEFORE_SURVEY_END){
-            return $state.go("tutorial");
-        }else if($scope.questionNum === AFTER_SURVEY_END){
-            return $state.go("end");
-        }else{
-            return $state.go("survey", {questionNum: $scope.questionNum + 1});
-        }
+        $scope.fadeOutQuestion($scope.qNums[$scope.questionNum], function(){
+            if($scope.questionNum === BEFORE_SURVEY_END){
+                return $state.go("tutorial");
+            }else if($scope.questionNum === AFTER_SURVEY_END){
+                return $state.go("end");
+            }else{
+                return $state.go("survey", {questionNum: $scope.questionNum + 1});
+            }
+        });
+
 
     };
 
